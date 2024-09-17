502d8cae6b3efc3b70d2b27e6f83d2c3ad3aee7a
Shahid9527 <sshahid3102000@gmail.comgit config --global user.name>
2024-09-17 14:55:38 +0530

    changed bg of breadcrumb and other minor changes

--- src/app/history/page.tsx
+++ src/app/history/page.tsx
@@ -18,7 +18,7 @@ const HistoryPage = () => {
         pageName="History"
         description=""
       />
-      <section className="pb-[120px]">
+      <section className="pb-[120px] mt-10">
         <div className="container">
           <div className="-mx-4 flex flex-wrap justify-center">
             <div className="w-full px-4 lg:w-10/12">
